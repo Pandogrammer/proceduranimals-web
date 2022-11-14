85c3ccb67ef6c9f41efa96de84778cb6f590eb35
fernando.hermes <fernando.hermes@etermax.com>
2022-11-14 12:47:55 -0300

    App levantada

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+
+let mensaje = ""
+
+let telegramBotId = "2031201399:AAGXQG0XVXhp9zQsRhAODEpyBRbKLKMhTAA"
+let chatId = "504832198L"
+
+function sendRequest (url) {
+  let baseUrl = `https://api.telegram.org/bot${telegramBotId}`
+  let requestUrl = `${baseUrl}/sendMessage?chat_id=${chatId}&text=${mensaje}`
+  return fetch(requestUrl)
+}
+
+function enviarMensaje(evento){
+  console.log(mensaje)
+  sendRequest(mensaje).then(e => console.log(e))
+}
+
+
+function onTextChange(evento){
+  mensaje = evento.target.value
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <textarea rows="4" cols="50" onChange={onTextChange}></textarea>
+        <input type="button" value="enviar" onClick={enviarMensaje}>
+        </input>
       </header>
     </div>
   );
 }
+
+
 
 export default App;
